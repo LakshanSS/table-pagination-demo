1a81e81aa6ab0cb57f02d4c7316ddb55a69d9877
lakshanss <lakshan230897@gmail.com>
2020-11-11 11:36:10 +0530

    Remove unwanted new line

--- src/ViewTable.js
+++ src/ViewTable.js
@@ -12,7 +12,6 @@ export default function ViewTable() {
     const [pageSize, setPageSize] = useState(10);
     const [isLoading, setIsLoading] = useState(false);
 
-
     useEffect(() => {
             getData();
         }, [pageSize, currentPage]
